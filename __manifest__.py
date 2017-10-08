ec64c87d130baa27e9c363e9308f4aa4f1b1653f
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2017-10-09 06:07:42 +0800

    Chnages Commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 {
     'name': "Globpak",
@@ -51,58 +50,4 @@
     'demo': [
         # 'demo/demo.xml',
     ],
-=======
-# -*- coding: utf-8 -*-
-{
-    'name': "Globpak",
-
-    'summary': """
-        Globpak Customizations""",
-
-    'description': """
-        - Expense Report
-        - Account Payable Voucher
-        - Check Print
-        - Check Voucher
-        - Sales Invoice
-    """,
-
-    'author': "Excode Innovations Inc.",
-    'website': "http://www.excodeinnovations.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Custom',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'hr_expense', 'report', 'l10n_us_check_printing'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        # 'views/views.xml',
-        # 'views/templates.xml',
-        'data/us_check_printing.xml',
-        'views/layout_templates.xml',
-        'views/account_invoice_view.xml',
-        'views/hr_expense_view.xml',
-        'views/report_expense_sheet.xml',
-        'views/report_account_payable_voucher.xml',
-        'views/report_invoice.xml',
-        'report/print_check.xml',
-        'report/print_check_top.xml',
-        'report/print_check_bottom.xml',
-        'views/report_collection_receipt.xml',
-        'views/report_journal_voucher.xml',
-        'views/report_credit_memo.xml',
-        'views/report_debit_memo.xml',
-        'views/report.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        # 'demo/demo.xml',
-    ],
->>>>>>> 38d76b6e2eecc31ac32a4e9d1250e6a4f64b8912
 }
--- models/account_invoice.py
+++ models/account_invoice.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from odoo import models, fields, api
 
 class AccountInvoice(models.Model):
@@ -50,56 +49,3 @@ class AccountInvoice(models.Model):
 
     # OVERRIDE FIELDS
     comment = fields.Text(default="All accounts are payable on the terms stated above. Interest of 36% per annum will be charged on all overdue counts. All claims of corrections to invoice must be made within two days of receipt of goods. Parties  expressly submit to the jurisdiction of the courts of Paranaque City on any legal action arrising from this transaction and an additional sum equal to twenty-five 25 percent of the amount due will be charge for attorney's fees and other costs.")
-=======
-from odoo import models, fields, api
-
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
-
-    @api.one
-    @api.depends('invoice_line_ids', 'invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'currency_id', 'company_id', 'date_invoice', 'type')
-    def _compute_amount_sales(self):
-        vat_sales = 0
-        vat_exempt = 0
-        zero_rated = 0
-        for line in self.invoice_line_ids:
-            if line.invoice_line_tax_ids:
-                for tax in line.invoice_line_tax_ids:
-                    # Check if zero rated sales or vatable sales
-                    if tax.amount <= 0:
-                        # Zero Rated Sales
-                        zero_rated += line.price_subtotal
-                    else:
-                        # Vatable Sales
-                        vat_sales += line.price_subtotal
-            else:
-                # Vat Exempt Sales
-                vat_exempt += line.price_subtotal
-
-        sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
-
-        self.vat_sales = vat_sales
-        self.vat_exempt_sales = vat_exempt
-        self.zero_rated_sales = zero_rated
-
-        self.vat_sales_signed = vat_sales * sign
-        self.vat_exempt_sales_signed = vat_exempt * sign
-        self.zero_rated_sales_signed = zero_rated * sign
-
-    vat_sales = fields.Monetary(string='Vatable Sales', store=True, readonly=True, compute='_compute_amount_sales', track_visibility='always')
-    vat_exempt_sales = fields.Monetary(string='Vat Exempt Sales', store=True, readonly=True, compute='_compute_amount_sales', track_visibility='always')
-    zero_rated_sales = fields.Monetary(string='Zero Rated Sales', store=True, readonly=True, compute='_compute_amount_sales', track_visibility='always')
-
-    vat_sales_signed = fields.Monetary(string='Vatable Sales Signed', store=True, readonly=True, compute='_compute_amount_sales', track_visibility='always')
-    vat_exempt_sales_signed = fields.Monetary(string='Vat Exempt Sales Signed', store=True, readonly=True, compute='_compute_amount_sales', track_visibility='always')
-    zero_rated_sales_signed = fields.Monetary(string='Zero Rated Sales Signed', store=True, readonly=True, compute='_compute_amount_sales', track_visibility='always')
-
-    po_no = fields.Char(string='PO No.')
-    dr_no = fields.Char(string='DR No.')
-    dr_date = fields.Datetime(string='DR Date')
-
-    x_approved_by = fields.Many2one('res.partner', 'Approved By')
-
-    # OVERRIDE FIELDS
-    comment = fields.Text(default="All accounts are payable on the terms stated above. Interest of 36% per annum will be charged on all overdue counts. All claims of corrections to invoice must be made within two days of receipt of goods. Parties  expressly submit to the jurisdiction of the courts of Paranaque City on any legal action arrising from this transaction and an additional sum equal to twenty-five 25 percent of the amount due will be charge for attorney's fees and other costs.")
->>>>>>> 38d76b6e2eecc31ac32a4e9d1250e6a4f64b8912
