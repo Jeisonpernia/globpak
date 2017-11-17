a8dbb39047a0f95afd5755c79bd4a630bb7d5af5
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2017-11-17 16:20:51 +0800

    Commit Current Changes

--- __init__.py
+++ __init__.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 from . import controllers
 from . import models
 from . import report
-=======
-# -*- coding: utf-8 -*-
-
-from . import controllers
-from . import models
-from . import report
->>>>>>> 80172243da9ab8c20a53be2ffaaf0487131cbbeb
 from . import wizard
